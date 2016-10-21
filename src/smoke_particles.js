942ceeb3fd066824195773176c96941dafaf7a59
Dylan-George <dgeorge000@gmail.com>
2016-10-21 15:14:10 -0500

    Add files via upload

---
+++ src/smoke_particles.js
@@ -0,0 +1,90 @@
+"use strict";
+
+/**
+ * @module BulletPool
+ * A class for managing a particle engine that 
+ * emulates a smoke trail
+ */
+module.exports = exports = SmokeParticles;
+
+/**
+ * @constructor SmokeParticles
+ * Creates a SmokeParticle of the specified size
+ * @param {uint} size the maximum number of particles to exits concurrently
+ */
+function SmokeParticles(maxSize) {
+  this.pool = new Float32Array(4 * maxSize);
+  this.start = 0;
+  this.end = 0;
+  this.max = maxSize;
+}
+
+/**
+ * @function emit
+ * @param {Vector} position where the bullet begins
+*/
+SmokeParticles.prototype.emit = function(position) {
+	
+	this.pool[4*this.end] = position.x;
+	this.pool[4*this.end+1] = position.y;
+	this.pool[4*this.end+2] = 0.0;
+	this.end++;
+  }
+}
+
+/**
+ * @function update
+ * Updates the bullet using its stored velocity, and
+ * calls the callback function passing the transformed
+ * bullet.  If the callback returns true, the bullet is
+ * removed from the pool.
+ * Removed bullets are replaced with the last bullet's values
+ * and the size of the bullet array is reduced, keeping
+ * all live bullets at the front of the array.
+ * @param {DOMHighResTimeStamp} elapsedTime
+ * @param {function} callback called with the bullet's position,
+ * if the return value is true, the bullet is removed from the pool
+ */
+BulletPool.prototype.update = function(elapsedTime, callback) {
+  for(var i = 0; i < this.end; i++){
+    // Move the bullet
+    this.pool[4*i] += this.pool[4*i+2];
+    this.pool[4*i+1] += this.pool[4*i+3];
+    // If a callback was supplied, call it
+    if(callback && callback({
+      x: this.pool[4*i],
+      y: this.pool[4*i+1]
+    })) {
+      // Swap the current and last bullet if we
+      // need to remove the current bullet
+      this.pool[4*i] = this.pool[4*(this.end-1)];
+      this.pool[4*i+1] = this.pool[4*(this.end-1)+1];
+      this.pool[4*i+2] = this.pool[4*(this.end-1)+2];
+      this.pool[4*i+3] = this.pool[4*(this.end-1)+3];
+      // Reduce the total number of bullets by 1
+      this.end--;
+      // Reduce our iterator by 1 so that we update the
+      // freshly swapped bullet.
+      i--;
+    }
+  }
+}
+
+/**
+ * @function render
+ * Renders all bullets in our array.
+ * @param {DOMHighResTimeStamp} elapsedTime
+ * @param {CanvasRenderingContext2D} ctx
+ */
+BulletPool.prototype.render = function(elapsedTime, ctx) {
+  // Render the bullets as a single path
+  ctx.save();
+  ctx.beginPath();
+  ctx.fillStyle = "black";
+  for(var i = 0; i < this.end; i++) {
+    ctx.moveTo(this.pool[4*i], this.pool[4*i+1]);
+    ctx.arc(this.pool[4*i], this.pool[4*i+1], 2, 0, 2*Math.PI);
+  }
+  ctx.fill();
+  ctx.restore();
+}
